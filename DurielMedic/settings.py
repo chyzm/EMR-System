05950c2423185331e19859f904ece9cd767b944d
chyzm <somtochiokaro@gmail.com>
2025-08-09 16:18:46 +0100

    fixed allowed host

--- DurielMedic/settings.py
+++ DurielMedic/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'durielmedic.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1']
 
 
 # Application definition

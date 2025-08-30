e32f9bcd120ce8723019eb980e0c27ffeb4f85dd
chyzm <somtochiokaro@gmail.com>
2025-08-30 04:18:16 +0100

    home page

--- DurielMedic/settings.py
+++ DurielMedic/settings.py
@@ -31,7 +31,7 @@ if not SECRET_KEY:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 # DEBUG = os.getenv('DEBUG') 
 
 
@@ -101,36 +101,36 @@ WSGI_APPLICATION = 'DurielMedic.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'duriel_med',
-        'USER': 'postgres',
-        'PASSWORD': 'Legacy@90',
-        'HOST': 'localhost',
-        'PORT': 5433,
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'duriel_med',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Legacy@90',
+#         'HOST': 'localhost',
+#         'PORT': 5433,
+#     }
+# }
+
+
+
+
+if os.getenv("DEBUG") == "True":  # Local/PostgreSQL
+    DATABASES = {
+        # "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+         "default": dj_database_url.config(conn_max_age=600)
     }
-}
-
-
-
-
-# if os.getenv("DEBUG") == "True":  # Local/PostgreSQL
-#     DATABASES = {
-#         # "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
-#          "default": dj_database_url.config(conn_max_age=600)
-#     }
-# else:  # PythonAnywhere/MySQL
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': os.getenv('DB_ENGINE'),
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT'),
-#         }
-#     }
+else:  # PythonAnywhere/MySQL
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
     
     
 
@@ -156,27 +156,27 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 
 
-# # LOGS_DIR = BASE_DIR / "logs"
-# # LOGS_DIR.mkdir(exist_ok=True)  # ✅ auto-create logs folder if missing
+LOGS_DIR = BASE_DIR / "logs"
+LOGS_DIR.mkdir(exist_ok=True)  # ✅ auto-create logs folder if missing
 
-# # LOGGING = {
-# #     'version': 1,
-# #     'disable_existing_loggers': False,
-# #     'handlers': {
-# #         'file': {
-# #             'level': 'WARNING',
-# #             'class': 'logging.FileHandler',
-# #             'filename': LOGS_DIR / 'django.log',
-# #         },
-# #     },
-# #     'loggers': {
-# #         'django': {
-# #             'handlers': ['file'],
-# #             'level': 'WARNING',
-# #             'propagate': True,
-# #         },
-# #     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': LOGS_DIR / 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
 
 
 

edc4b885eae41a1fd439e62a20b5d444a0392cd0
chyzm <somtochiokaro@gmail.com>
2026-01-29 11:11:53 +0100

    Feature: Lab-1

--- DurielMedic/settings.py
+++ DurielMedic/settings.py
@@ -113,36 +113,36 @@ CHANNEL_LAYERS = {
 #Database
 #https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'duriel_med',
-        'USER': 'postgres',
-        'PASSWORD': 'Legacy@90',
-        'HOST': 'localhost',
-        'PORT': 5433,
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'duriel_med',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Legacy@90',
+#         'HOST': 'localhost',
+#         'PORT': 5433,
+#     }
+# }
+
+
+
+
+if os.getenv("DEBUG") == "True":  # Local/PostgreSQL
+    DATABASES = {
+        # "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+         "default": dj_database_url.config(conn_max_age=600)
     }
-}
-
-
-
-
-# if os.getenv("DEBUG") == "True":  # Local/PostgreSQL
-#     DATABASES = {
-#         # "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
-#          "default": dj_database_url.config(conn_max_age=600)
-#     }
-# else:  # PythonAnywhere/MySQL
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': os.getenv('DB_ENGINE'),
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT'),
-#         }
-#     }
+else:  # PythonAnywhere/MySQL
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
     
     
 
